871ae711c063cb2ab9a9351deedb358a8b8cd75c
xuegao111222 <1093352488@qq.com>
2026-09-03 21:34:59 +0800

    trackerbar组件

---
+++ leran opencv/trackbar.py
@@ -0,0 +1,29 @@
+import cv2
+import numpy  as np
+def callback():
+    pass
+
+cv2.namedWindow('trackbar',cv2.WINDOW_NORMAL)
+
+#创建trackbar
+cv2.createTrackbar('R','trackbar',0,255,callback)
+cv2.createTrackbar('G','trackbar',0,255,callback)
+cv2.createTrackbar('B','trackbar',0,255,callback)
+
+#创建一个背景图片
+img = np.zeros((480,640,3),np.uint8)
+while True:
+    cv2.imshow('trackbar',img)
+
+    #获取当前trackbar的值 
+    r = cv2.getTrackbarPos('R','trackbar')
+    g = cv2.getTrackbarPos('G','trackbar')
+    b = cv2.getTrackbarPos('B','trackbar')
+    #改变背景图片颜色
+    img[:]=[b,g,r] #因为上色是b g r
+
+    key = cv2.waitKey(10)
+    if key & 0xFF == ord('q'):
+        break
+
+cv2.destroyAllWindows()
